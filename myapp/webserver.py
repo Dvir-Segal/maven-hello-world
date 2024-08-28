2c97495c81ef20f21786c03b6efe3b47ad504946
Dvir-Segal <74387464+Dvir-Segal@users.noreply.github.com>
2024-08-28 14:33:46 +0300

    Update webserver.py

--- myapp/webserver.py
+++ myapp/webserver.py
@@ -1,11 +1,14 @@
-from flask import Flask, send_file
+from flask import Flask
+import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Serve the log file
-    return send_file('/app/log.txt', mimetype='text/plain')
+    # Execute the JAR file and get output
+    result = subprocess.run(['java', '-jar', 'myapp-{}-SNAPSHOT.jar'.format(env.new_version)], capture_output=True, text=True)
+    # Return the output
+    return f"<pre>{result.stdout}</pre>"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=9090)
+    app.run(host='0.0.0.0', port=8080)
