98f749fe3798fd42b251bf273428e51545b82879
Dvir-Segal <74387464+Dvir-Segal@users.noreply.github.com>
2024-08-29 01:51:15 +0300

    Update App.java

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Dvir Segal!" );
+        System.out.println( "Hello World , this is Dvir Segal!" );
     }
 }
